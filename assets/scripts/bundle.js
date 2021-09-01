ba725385aeda70b8b0f39bad5358af76606a8de5
medeirosinacio <medeirosinacio@outlook.com>
2021-09-01 12:18:17 -0300

    install AOS library and add landing page effect

---
+++ assets/scripts/bundle.js
@@ -0,0 +1,7 @@
+---
+layout: js_minifier
+replace_names: false
+---
+
+{% include_relative aos/aos.js %}
+{% include_relative default.js %}
---
+++ assets/scripts/default.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 400,
+        easing: 'ease-in-out',
+    });
+});
